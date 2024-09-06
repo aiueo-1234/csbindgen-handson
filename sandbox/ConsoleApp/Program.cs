7956775104b54aeef0ae030851bd20b814ab741a
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-09-07 05:29:37 +0900

    call myMath_add

--- sandbox/ConsoleApp/Program.cs
+++ sandbox/ConsoleApp/Program.cs
@@ -2,8 +2,4 @@
 
 var c = new BasicFunctionCall();
 c.CallRustAdd(1,1);
-// c.CallRustPow(2,3);
-// c.CallMyMathAdd(2,2);
-
-// using var t = new TestGroupedNativeMethods();
-// t.PushAndPop([1,2,3,4,5]);
+c.CallMyMathAdd(2,2);
--- src/CsbindgenHandsOn/BasicFunctionCall.cs
+++ src/CsbindgenHandsOn/BasicFunctionCall.cs
@@ -6,12 +6,8 @@ public class BasicFunctionCall
         Console.WriteLine($"rust_add({a}, {b}): {Native.NativeMethods.rust_add(a,b)}");
     }
 
-    // public void CallRustPow(int a, int b){
-    //     Console.WriteLine($"rust_pow({a}, {b}): {Native.NativeMethods.rust_pow(a,b)}");
-    // }
-
-    // public void CallMyMathAdd(int a, int b){
-    //     Console.WriteLine($"myMath_add({a}, {b}): {Native.CNativeMethodsMyMath.myMath_add(a,b)}");
-    // }
+    public void CallMyMathAdd(int a, int b){
+        Console.WriteLine($"myMath_add({a}, {b}): {Native.CNativeMethodsMyMath.myMath_add(a,b)}");
+    }
 }
 
