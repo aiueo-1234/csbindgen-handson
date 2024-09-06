092ed9d81e75a06cef80d616f1f8c68416bb8bf1
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-09-07 05:01:51 +0900

    add myMath.c/.h

---
+++ src/libcsbindgenhandson/src/c/myMath.c
@@ -0,0 +1,9 @@
+#include "myMath.h"
+
+int myMath_mul(int a, int b){
+    return a * b;
+}
+
+int myMath_add(int a, int b){
+    return a + b;
+}
---
+++ src/libcsbindgenhandson/src/c/myMath.h
@@ -0,0 +1,7 @@
+#ifndef MYMATH_H_
+#define MYMATH_H_
+
+int myMath_mul(int a, int b);
+int myMath_add(int a, int b);
+
+#endif
