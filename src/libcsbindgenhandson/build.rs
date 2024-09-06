a98ed68d172283938d4e177977e1d8107a0ede61
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-09-07 05:02:20 +0900

    generate ffi code for rust

--- src/libcsbindgenhandson/build.rs
+++ src/libcsbindgenhandson/build.rs
@@ -1,4 +1,13 @@
 fn main(){
+    bindgen::Builder::default()
+        .header("src/c/myMath.h")
+        .generate().unwrap()
+        .write_to_file("src/myMath.rs").unwrap();
+
+    cc::Build::new()
+        .file("src/c/myMath.c")
+        .try_compile("myMath").unwrap();
+    
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("csbindgenhandson")
